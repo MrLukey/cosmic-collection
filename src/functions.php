06afb7e39ce5ff4264ae0673b8f39b4fa34a7036
Luke Landau <luke.a.landau@gmail.com>
2021-08-04 13:59:06 +0100

    Removed requirement for a cosmic event to produce elements

--- src/functions.php
+++ src/functions.php
@@ -11,7 +11,7 @@ function format_event_elements(array $elements): array {
 
 function display_cosmic_event(array $db_row, array $event_elements): string {
 	if (!array_key_exists('name', $db_row) || !array_key_exists('caused_by', $db_row) ||
-		!array_key_exists('creates_effect', $db_row) || count($event_elements) < 1)
+		!array_key_exists('creates_effect', $db_row))
 		return 'FAIL';
 	$html = '<div class="cosmic_event"><h2>' . $db_row['name'] . '</h2><h3>Caused by: ' . $db_row['caused_by']
 		. '</h3><h3>Creates: ' . $db_row['creates_effect'] . '</h3><h3>Produces:</h3><p>';
